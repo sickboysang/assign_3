be263011317e1151045aa9229f6801b1e6d35a5a
sickboysang <144414737+sickboysang@users.noreply.github.com>
2023-11-15 11:53:37 -0500

    updatedhello

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
 //imports
-const express = require('express')
+const express = require('express');
+const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const app = express();
-const port = 4000
-
+const port = 4000;
+const routes=require("./server/routes/routes.js");
 mongoose.connect('mongodb://localhost:27017/node',{useNewUrlParser: true,useUnifiedTopology: true});
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "MongoDB connection"));
-db.once("open", () => {
-  console.log("database connected")
+db.once("open", () => {;
+  console.log("database connected");
 });
 
-app.set('views', './views')
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname+ 'public/css/'))
-app.use('/js', express.static(__dirname+ 'public/js'))
-app.use('/img', express.static(__dirname+ 'public/img'))
+app.use('/',routes);
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.set('layouts','./layouts/home');
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname+ 'public/css/'));
+app.use('/js', express.static(__dirname+ 'public/js'));
+app.use('/img', express.static(__dirname+ 'public/img'));
 
 app.get('', (req, res) => {
-  res.render('home', {text: 'testing from index file'})
+  res.render('home', {text: 'testing from index file'});
 });
 
 app.listen(port, () => {
