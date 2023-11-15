6e310eef74e611815513bba746d39ad6d291cc9b
sickboysang <144414737+sickboysang@users.noreply.github.com>
2023-11-15 12:26:06 -0500

    updateedd

--- app.js
+++ app.js
@@ -4,20 +4,23 @@ const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const app = express();
+
 const port = 4000;
-const routes=require("./server/routes/routes.js");
+const routes=require("./servSer/routes/routes.js");
 mongoose.connect('mongodb://localhost:27017/node',{useNewUrlParser: true,useUnifiedTopology: true});
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "MongoDB connection"));
-db.once("open", () => {;
+db.once("open", () => {
   console.log("database connected");
 });
 
-app.use('/',routes);
+app.use('/', routes);
+
 app.set('views', './views');
 app.set('view engine', 'ejs');
-app.set('layouts','./layouts/home');
+app.set('layout','./layouts/home');
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname+ 'public/css/'));
 app.use('/js', express.static(__dirname+ 'public/js'));
