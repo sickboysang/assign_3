7f66679855f0d22b6b434a90dc96af5035cd1696
sickboysang <144414737+sickboysang@users.noreply.github.com>
2023-11-14 23:40:57 -0500

    updated hello

---
+++ app.js
@@ -0,0 +1,29 @@
+//imports
+const express = require('express')
+const mongoose = require("mongoose");
+const session = require("express-session");
+const app = express();
+const port = 4000
+
+mongoose.connect('mongodb://localhost:27017/node',{useNewUrlParser: true,useUnifiedTopology: true});
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection"));
+db.once("open", () => {
+  console.log("database connected")
+});
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname+ 'public/css/'))
+app.use('/js', express.static(__dirname+ 'public/js'))
+app.use('/img', express.static(__dirname+ 'public/img'))
+
+app.get('', (req, res) => {
+  res.render('home', {text: 'testing from index file'})
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
